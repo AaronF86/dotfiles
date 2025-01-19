eb50e78dda3c9ee7a5cd68b399204e51c2848a17
aaron <aaronfulton86@gmail.com>
2025-01-19 16:00:28 +0000

    fix: laptop added boot

--- nixOS/hosts/laptop.nix
+++ nixOS/hosts/laptop.nix
@@ -5,7 +5,16 @@
     ../modules/kde.nix
     ../modules/terminal.nix
     ../modules/development.nix
+    ../modules/gaming.nix
     ../modules/communication.nix
-    ./hardware-configuration-laptop.nix
+    ./hardware-configuration-desktop.nix
   ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi = {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/boot";
+  };
+
+  system.stateVersion = "24.11";
 }
