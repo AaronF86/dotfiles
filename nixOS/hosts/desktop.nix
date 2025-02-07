a548838e1d2714abe85ed577fdf15a1f9393db3a
aaron <aaronfulton86@gmail.com>
2025-02-07 08:50:24 +0000

    fix: ssh

--- nixOS/hosts/desktop.nix
+++ nixOS/hosts/desktop.nix
@@ -39,8 +39,8 @@
   # System Version
   system.stateVersion = "24.11";
 
-    environment.etc."ssh/id_ed25519_desktop".source = "/home/your_user/.ssh/id_ed25519_desktop";
-  environment.etc."ssh/id_ed25519_desktop.pub".source = "/home/your_user/.ssh/id_ed25519_desktop.pub";
+    environment.etc."ssh/id_ed25519_desktop".source = "/home/aaron/.ssh/id_ed25519_desktop";
+  environment.etc."ssh/id_ed25519_desktop.pub".source = "/home/aaron/.ssh/id_ed25519_desktop.pub";
 
   environment.etc."ssh/config".text = ''
     Host github.com
