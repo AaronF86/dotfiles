bf03a3382f8e11c4131693beb7a5f01a110bb279
aaron <aaronfulton86@gmail.com>
2025-09-25 13:53:49 +0000

    Feat: added Common Options

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # Aaron's Dotfiles V4 
 
 This is my 4th attempt at creating some dotfiles in nix. let hope I can keep this one for more then a few months.
+
+## Goals
+
+- Create a nix config that can be used across multiple devices
+- Have the ability to add new users easily (despite not needing it really)
+- Be able to swap out config easily
+- just be kinda cool (in other words not simple)
+
+## Usage
+
+```bash
+git clone https://github.com/aaron/dotfiles.git
+cd dotfiles
+sudo nixos-rebuild switch --flake .#systemName
+```
+
+### New System Setup
+
+add new .nix file to machines directory copy from another machine and edit as needed
+
+### New User Setup
+
+add new .nix file to users directory copy from another user and edit as needed then add user to machine config in machines directory
+
+## Packages
+<Standard packages used across the aaron user config on most machines>
+
+- Browser: [Zen Browser](https://github.com/ZenBrowser)
+- Terminal: [Ghostty](https://github.com/ghostty/ghostty)
+- Shell: [fish](https://fishshell.com/)
+- Editor: [neovim](https://neovim.io/)
+
+- Status Bar: [waybar](https://github.com/swaywm/waybar)
+- Notification Daemon: [dunst](https://dunst-project.org/)
+- File Manager: [thunar](https://docs.xfce.org/xfce/thunar/start)
+- Launcher: [rofi](https://github.com/adi1090x/rofi)
+- Desktop: [hyprland](https://hyprland.org/)
+- Login Manager: [ly](https://github.com/fairyglade/ly)
+
+
+## Notes 
+
+- openJDK 8, 21, 26 are installed as gradle within jetbrains IDEs seems to have issues otherwise
--- flake.nix
+++ flake.nix
@@ -1,27 +1,14 @@
 {
-    description = "Aaron's Dotfiles V4 - being my system configuration";
-
-    # Some Nice stuff to make it easy to create
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";     # Keep everything pinned to the stable branch
-    inputs.unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # For when the stable stuff breaks (always, Im looking at you Vesktop)
-    inputs.flake-utils.url = "github:numtide/flake-utils";       # I have no idea what this does, but its in every flake I see 
-    inputs.home-manager.url = "github:nix-community/home-manager"; # User Management init :3
-    inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";      # pin home-manager to the same nixpkgs as the rest of the flake
-    inputs.zen-browser.url = "github:unofficial-zen-browser/zen-browser";
-    inputs.zen-browser.inputs.nixpkgs.follows = "nixpkgs";
-    inputs.eko.url = "github:Kyren223/eko";
+  description = "Aaron's Dotfiles V4 - being my system configuration";
 
 
-    outputs = { self, nixpkgs, ... }:
-    let
-      lib = nixpkgs.lib;
-      mkMachine = import ./lib/mkMachine.nix { inherit nixpkgs; };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.zen-browser.url = "github:0xc000022070/zen-browser-flake";
+  inputs.zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.eko.url = "github:Kyren223/eko";
 
-      machines = {
-        framework = import ./machines/laptop.nix;
-        desktop = import ./machines/laptop.nix;
-      };
-    in {
-      nixosConfigurations = lib.mapAttrs (_: mkMachine) machines;
-    };
+
 }
