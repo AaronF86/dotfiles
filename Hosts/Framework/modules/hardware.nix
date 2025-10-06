28cb2f0f44c1138b6130103197d8a4b4f30cc9cf
aaron <aaronfulton86@gmail.com>
2025-10-06 07:58:17 +0000

    feat: added HardwareConfig

---
+++ Hosts/Framework/modules/hardware.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [ "amd_pstate=active" ];
+    blacklistedKernelModules = [ "k10temp" ];
+    extraModulePackages = [ config.boot.kernelPackages.zenpower ];
+    kernelModules = [ "zenpower" ];
+  };
+
+  hardware = {
+    enableAllFirmware = true;
+    firmware = [ pkgs.linux-firmware ];
+    cpu.amd.updateMicrocode = true;
+    bluetooth.enable = true;
+    graphics.enable = true;
+    bolt.enable = true;
+  };
+
+  networking.networkmanager.enable = true;
+
+  services = {
+    tlp.enable = true;
+
+    xserver = {
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
+      libinput.enable = true;
+    };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+    };
+
+    fprintd.enable = true;
+    iio-sensor-proxy.enable = true;
+    logind.extraConfig = "HandleLidSwitch=suspend";
+    fstrim = {
+      enable = true;
+      interval = "weekly";
+    };
+  };
+
+    nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+    };
+    systemd.services.nix-gc.enable = false
+}
